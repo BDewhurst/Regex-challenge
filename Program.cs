10225cd2ecd8f5e443f3ea7ca5308e8edde892b6
Brad <bradleydewhurst123@outlook.com>
2023-11-21 10:12:47 +0000

    Regex

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System; 
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace RegularExpression {
+    class Program {
+        static void Main(string[] args) {
+                        string text = System.IO.File.ReadAllText(@"./input2.txt");
+                        string pattern = @"\d{2,3}";
+                        Regex regex = new Regex(pattern);
+                         MatchCollection matches = regex.Matches(text);
+                         foreach (Match match in matches) {
+                             int parsedNumber;
+                    if (int.TryParse(match.Value, out parsedNumber)) {
+                       char convertedChar = Convert.ToChar(parsedNumber);
+                       Console.WriteLine(convertedChar);
+                }
+                         }
+        }
+    }
+}
